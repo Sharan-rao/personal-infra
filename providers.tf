4ece4196304809317d5b31354134840272af789c
Sharan-rao <sharanrao85@gmail.com>
2024-10-02 23:58:32 +0530

    Initial proj commit - VPC

--- providers.tf
+++ providers.tf
@@ -5,6 +5,14 @@ terraform {
       version = "5.69.0"
     }
   }
+
+  backend "s3" {
+    bucket         = "bknd-tf"  # Name of the S3 bucket where the state will be stored.
+    key            = "state/terraform.tfstate" # Path within the bucket where the state will be read/written.
+    region         = "ap-south-1" # AWS region of the S3 bucket.
+    dynamodb_table = "terraform-lock" # DynamoDB table used for state locking.
+    encrypt        = true  # Ensures the state is encrypted at rest in S3.
+  }
 }
 
 provider "aws" {
